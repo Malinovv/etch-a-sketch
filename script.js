3749333fe78fa87936dc4b7ea403c4abe47b77e4
Kristijan Malinov <kristijan.malinovv@gmail.com>
2024-01-17 01:24:47 +0100

    final project build with range func and color picker

--- script.js
+++ script.js
@@ -1,28 +1,51 @@
 const parentContainer = document.querySelector(".parent");
 const resetBtn = document.querySelector("#reset");
+const gridSizeInput = document.querySelector("#gridSize");
+const colorPickerInput = document.querySelector("#colorPicker");
 
-function createDivs() {
-  for (let i = 0; i < 256; i++) {
+function createDivs(gridItems) {
+  parentContainer.innerHTML = "";
+  const gridPattern = gridItems * gridItems;
+
+  const childWidth = 24 / gridItems;
+  const childHeight = 24 / gridItems;
+
+  for (let i = 0; i < gridPattern; i++) {
     const childDiv = document.createElement("div");
     childDiv.setAttribute("class", "child");
+    childDiv.style.width = `calc(${childWidth}em)`;
+    childDiv.style.height = `calc(${childHeight}em)`;
     parentContainer.appendChild(childDiv);
   }
+
+  const children = document.querySelectorAll(".child");
+  children.forEach((child) => {
+    child.addEventListener("mouseover", changeColor);
+  });
 }
-
-createDivs();
-
-const children = document.querySelectorAll(".child");
 
 function changeColor() {
-  this.style.backgroundColor = "red";
+  const selectedColor = colorPickerInput.value;
+  this.style.backgroundColor = selectedColor;
 }
 
-children.forEach((child) => {
-  child.addEventListener("mouseover", changeColor);
-});
-
-resetBtn.addEventListener("click", () => {
+function resetColors() {
+  const children = document.querySelectorAll(".child");
   children.forEach((child) => {
     child.style.backgroundColor = "aqua";
   });
+}
+
+createDivs(gridSizeInput.value);
+
+resetBtn.addEventListener("click", resetColors);
+
+gridSizeInput.addEventListener("input", () => {
+  const gridItems = gridSizeInput.value;
+  createDivs(parseInt(gridItems));
+});
+
+colorPickerInput.addEventListener("input", () => {
+  const gridItems = gridSizeInput.value;
+  createDivs(parseInt(gridItems));
 });
